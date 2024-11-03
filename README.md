b9a0f888ab5aefdb60391066ec2fdcb5c90a1222
Sixuan Wu <wusixuan2@icloud.com>
2024-11-02 20:52:34 -0400

    add env var config

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ python3 -m venv venv
 source venv/bin/activate
 python -m pip install --upgrade pip
 pip install -r requirements.txt
+mkdir -p .streamlit/ && touch .streamlit/secrets.toml
+```
+Add `BACKEND_URL="YOUR_BACKEND_URL"` to `.streamlit/secrets.toml`
+```
 streamlit run app.py
 ```
 Local URL: http://localhost:8501
--- app.py
+++ app.py
@@ -2,3 +2,13 @@ import streamlit as st
 import requests
 
 uploaded_file = st.file_uploader(label="Choose a PDF file", type="pdf")
+
+backend_url = st.secrets["BACKEND_URL"]
+
+if uploaded_file is not None and backend_url:
+    response = requests.post(backend_url, json={'pdf_url': 'test'})
+    
+    if response.status_code == 200:
+        st.success("PDF uploaded successfully!")
+    else:
+        st.error("Failed to upload PDF.")
